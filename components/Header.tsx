0c3bc3f453945108f982f6b2eca39c5fc0c717bb
imen_aouissi22 <imenneaouissii@gmail.com>
2025-11-05 10:39:00 +0100

    feat(ui): refine header and mobile menu styling for better responsiveness

--- components/Header.tsx
+++ components/Header.tsx
@@ -106,12 +106,12 @@ export default function Header() {
 
               {/* Menu Area */}
               <div className="menu-area clearfix">
-                {/* Mobile Toggler */}
-                <div className="mobile-nav-toggler">
-                  <i className="icon-bar"></i>
-                  <i className="icon-bar"></i>
-                  <i className="icon-bar"></i>
-                </div>
+                {/* Mobile Toggler – Triggers menu via body class */}
+<div className="mobile-nav-toggler" onClick={() => document.body.classList.add('mobile-menu-visible')}>
+  <i className="icon-bar"></i>
+  <i className="icon-bar"></i>
+  <i className="icon-bar"></i>
+</div>
 
                {/* Main Menu */}
 <nav className="main-menu navbar-expand-md navbar-light">
